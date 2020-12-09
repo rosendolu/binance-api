f844c47b938dee19ce31912deb6203a34b8f8415
rosendolu <rosendo2018@163.com>
2020-12-09 10:46:15 +0800

    chore:

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
-const targetURl = 'https://fapi.binance.com'
+// const targetURl = 'http://fapi.binance.com'
+const targetURl = 'http://192.168.3.202:8080/'
 
 const app = express()
+
+app.get('/', (req, res) => {
+	res.send('success')
+})
 app.use(
-	'/fapi',
+	'/binance',
 	createProxyMiddleware({
 		target: targetURl,
 		changeOrigin: true,
+		pathRewrite: {
+			// '^/binance/fapi/': '/api/new-path', // rewrite path
+			'^/binance/fapi': '/fapi', // remove base path
+		},
 	})
 )
-app.listen(3000)
+app.listen(4000)
 
 // http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
